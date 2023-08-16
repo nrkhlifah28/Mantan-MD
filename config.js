9630b7650e02be4ff7b39f331019bc95d33cccec
FrierenDv <hackerxct007@gmail.com>
2023-08-16 12:02:24 +0700

    Initial Commit

--- config.js
+++ config.js
@@ -1,61 +1,38 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+import { watchFile, unwatchFile } from "fs";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
 
-// Owner
-global.owner = [
-['628xxx', 'Xyroinee', true]
-]
-global.mods = []
-global.prems = []
-// Info
-global.nomorwa = '628xxx'
-global.packname = 'anu - MD by'
-global.author = '© Xyroinee'
-global.namebot = '​anu - MD'
-global.wm = '© anu By Xyroine'
-global.stickpack = 'anu - MD by'
-global.stickauth = '© Xyroine'
-global.fotonya = 'https://telegra.ph/file/83b522fd80b660705d1cd.jpg'
-// Link Sosmed
-global.sig = 'https://www.instagram.com/xyroinee'
-global.sgh = 'https://github.com/Xyroinee'
-global.sgc = 'https://chat.whatsapp.com/'
-// Donasi
-global.psaweria = 'https://saweria.co/Zeltoria'
-global.ptrakterr = 'https://trakteer.id/zeltoria'
-global.povo = '0896-1075-0607'
-// Info Wait
-global.wait = 'Sedang Di Proses, Mohon Tunggu....'
-global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
-global.multiplier = 69 
-// Apikey
-global.lol = 'apikeymu'
-global.rose = 'apikeymu'
-global.xyro = 'apikeymu'
-// Catatan : Jika Mau Work Fiturnya
-// Masukan Apikeymu
-// Gapunya Apikey? Ya Daftar
-// Website: https://api.xyroinee.xyz
-// Daftar Ke Website Tersebut Untuk
-// Mendapatkan Apikey Kamu
-global.APIs = {
-    xyro: "https://api.xyroinee.xyz",
-    popcat : 'https://api.popcat.xyz'
+const configs = {
+  // Owner
+  owner: [["628xxx", "Xyroinee", true]],
+  mods: [],
+  prems: [],
+  // Info
+  packname: "anu - MD by",
+  author: "© Xyroinee",
+
+  wm: "© anu By Xyroine",
+  // Info Wait
+  wait: "Sedang Di Proses, Mohon Tunggu....",
+  eror: "Terjadi Kesalahan Coba Lagi Nanti!",
+  multiplier: 69,
+  APIs: {
+    rose: "https:/api.itsrose.life"
+  },
+
+  APIKeys: {
+    "https:/api.itsrose.life": "apikeymu",
+  },
 }
 
-/Apikey/
-global.APIKeys = {
-    "https://api.xyroinee.xyz": "apikeymu",
-}
+// Its should be ok, right?
+Object.assign(global, {
+  ...configs
+});
 
-let file = fileURLToPath(import.meta.url)
+const file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
-})
-
+	unwatchFile(file);
+	console.log(chalk.redBright("Update 'config.js'"));
+	import(`${file}?update=${Date.now()}`);
+});
